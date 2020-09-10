067dfb5ddad7c3f0762f6629bc21db6e0e601c9c
vetygustiawati <71057826+vetygustiawati@users.noreply.github.com>
2020-09-10 14:50:35 +0700

    belajar instance object

--- index.php
+++ index.php
@@ -18,4 +18,18 @@ $bmw->tipe = "320i";
 $bmw->mesin = "2000cc";
 $bmw->max_speed = "280km/h";
 
-echo "Tipe".$bmw->kecepatanMaksimal();
+$bmw2 = new Mobil;
+$bmw2->merk = "BMW";
+$bmw2->tipe = "320i";
+$bmw2->mesin = "2000cc";
+$bmw2->max_speed = "280km/h";
+
+echo "$bmw2->tipe";
+echo "<br>";
+echo "$bmw->tipe";
+
+if ($bmw === $bmw2) {
+	echo "Sama";
+}else {
+	echo "Tidak";
+}
