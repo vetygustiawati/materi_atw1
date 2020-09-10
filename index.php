d569184edc070e5f2b5e8b3b52afdbea65c1942b
vetygustiawati <71057826+vetygustiawati@users.noreply.github.com>
2020-09-10 15:10:41 +0700

    belajar inheritence

--- index.php
+++ index.php
@@ -6,30 +6,33 @@ class Mobil {
 	public function cetakTipe () {
 		return $this->tipe;
 	}
+
 	function kecepatanMaksimal () {
 		return "Kecepatan Maksimal Dari Mobil Ini Adalah ".$this ->max_speed;
 	}
 }
 
+class BMW extends Mobil {
 
-$bmw = new Mobil;
+}
+
+class Tesla extends Mobil {
+	function selfParking () {
+		echo "Parkir Sendiri";
+	}
+} 
+
+
+$bmw = new BMW;
 $bmw->merk = "BMW";
 $bmw->tipe = "320i";
 $bmw->mesin = "2000cc";
 $bmw->max_speed = "280km/h";
 
-$bmw2 = new Mobil;
-$bmw2->merk = "BMW";
-$bmw2->tipe = "320i";
-$bmw2->mesin = "2000cc";
-$bmw2->max_speed = "280km/h";
+$tesla = new Tesla;
+$tesla->merk = "Tesla";
+$tesla->tipe = "Model X";
+$tesla->mesin = "Listrik";
+$tesla->max_speed = "280km/h";
 
-echo "$bmw2->tipe";
-echo "<br>";
-echo "$bmw->tipe";
-
-if ($bmw === $bmw2) {
-	echo "Sama";
-}else {
-	echo "Tidak";
-}
+echo $tesla->kecepatanMaksimal ();
